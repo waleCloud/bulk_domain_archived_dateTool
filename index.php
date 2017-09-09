38914f1d6b68e8b502f2396733efbc185f90b0cf
Bart Inyang <bartholomew.inyang@poisenigeria.org>
2017-09-09 09:47:33 +0100

    filter files dir

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if (isset($_POST['btn'])) {
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="public/js/bootstrap.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="submit.js"></script>
+	
 </head>
 <body>
 	<div class="container">
